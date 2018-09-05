8ed9590d932dda03b688a1264748b45e9c7a8d93
dEV <rahulatff@gmail.com>
2018-09-05 11:41:48 +0530

    Basic structure give to components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('White Pasta', 'This is a pasta with white sauce', 'https://bit.ly/2PAP1v6'),
+    new Recipe('Paneer Toast', 'Chilli Paneer Toast Sandwich is a delicious. ',
+  'https://bit.ly/2wCR2iy')
+];
 
   constructor() { }
 
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,10 @@
+export class Recipe {
+    public name: string;
+    public desc: string;
+    public imagePath: string;
+    constructor(n: string, d: string, i: string) {
+        this.name = n;
+        this.desc = d;
+        this.imagePath = i;
+    }
+}
